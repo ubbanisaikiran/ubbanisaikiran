589d27a1631d7ee76ba5b284e1e379b0c16192ec
subbani <subbani@linkfields.com>
2022-10-31 17:22:41 +0530

    getstart Initialization

---
+++ App/screens/Homescreen/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Homescreen() {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  )
+}
---
+++ App/screens/usage/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Usage() {
+  return (
+    <View>
+      <Text>Usage</Text>
+    </View>
+  )
+}
